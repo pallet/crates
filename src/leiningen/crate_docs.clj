cbc6859ff70d81edf15bf894cb08db69450df027
Hugo Duncan <hugo@hugoduncan.org>
2013-02-15 20:45:19 -0500

    Allow passing target directory to crate-docs task

--- src/leiningen/crate_docs.clj
+++ src/leiningen/crate_docs.clj
@@ -2,9 +2,18 @@
   "Generate Crate documents"
   (:require
    [clojure.java.io :refer [file]]
+   [leiningen.core.main :refer [debug]]
    [pallet.crate.generate-docs :refer [generate]]))
 
 (defn crate-docs
   "Generate crate docs"
   [{:keys [root] :as project} & args]
-  (generate (file root "target")))
+  (let [f (first args)
+        f (and f (file f))]
+    (when (and f (not (.isDirectory f)))
+      (throw (ex-info
+              (str "Specified target " (.getPath f) " is not a directory.")
+              {:f (.getAbsolutePath f)})))
+    (let [f (or f (doto (file root "target") (.mkdir)))]
+      (debug "Generating to" (.getPath f))
+      (generate f))))
