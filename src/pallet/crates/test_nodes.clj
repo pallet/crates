563c62fb7d8dff923e859297b30835eecc06cf0e
Hugo Duncan <hugo@hugoduncan.org>
2013-03-15 22:29:36 -0400

    Add more vmfest images

--- src/pallet/crates/test_nodes.clj
+++ src/pallet/crates/test_nodes.clj
@@ -5,13 +5,27 @@
   "Provides the node specs for testing pallet crates."
   {:vmfest
    {:variants
-    [{:node-spec
+    [ ;; https://s3.amazonaws.com/vmfest-images/ubuntu-12.04.vdi.gz
+     {:node-spec
       {:image {:os-family :ubuntu :os-version-matches "12.04"
                :os-64-bit true}}
       :group-suffix "u1204"
       :selectors #{:default :all :ubuntu :ubuntu-12 :ubuntu-12-04}}
+     ;; https://s3.amazonaws.com/vmfest-images/ubuntu-10-10-64bit-server.vdi.gz
+     {:node-spec
+      {:image {:os-family :ubuntu :os-version-matches "10.10"
+               :os-64-bit true}}
+      :group-suffix "u1010"
+      :selectors #{::all :ubuntu :ubuntu-12 :ubuntu-10-10}}
+     ;; https://s3.amazonaws.com/vmfest-images/debian-6.0.2.1-64bit-v0.3.vdi.gz
      {:node-spec
       {:image {:os-family :debian :os-version-matches "6.0.2.1"
                :os-64-bit true}}
-      :group-suffix "d60"
+      :group-suffix "d602"
+      :selectors #{:all :debian :debian-6 :debiab-6-0}}
+     ;; https://s3.amazonaws.com/vmfest-images/Debian-6.0.1a-64bit.vdi.gz
+     {:node-spec
+      {:image {:os-family :debian :os-version-matches "6.0.1.*"
+               :os-64-bit true}}
+      :group-suffix "d601"
       :selectors #{:all :debian :debian-6 :debiab-6-0}}]}})
