04dcfd778fb6ea7891525ffa6db84f2893562e53
Hugo Duncan <hugo@hugoduncan.org>
2014-10-04 07:52:05 -0400

    Update version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/crates "0.1.2-SNAPSHOT"
+(defproject com.palletops/crates "0.1.3-SNAPSHOT"
   :description "Information for the pallet crates."
   :url "http://palletops.com"
   :license {:name "Eclipse Public License"
